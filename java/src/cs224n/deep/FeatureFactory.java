f24d072ce36fa114f13a11434240b4aef70b1497
Andrew Francis Giel <agiel@stanford.edu>
2014-11-20 17:37:36 -0800

    added Feature Factory funcs

--- java/src/cs224n/deep/FeatureFactory.java
+++ java/src/cs224n/deep/FeatureFactory.java
@@ -31,11 +31,15 @@ public class FeatureFactory {
 	
 	private static List<Datum> read(String filename)
 			throws FileNotFoundException, IOException {
-	    // TODO: you'd want to handle sentence boundaries
 		List<Datum> data = new ArrayList<Datum>();
 		BufferedReader in = new BufferedReader(new FileReader(filename));
+    Datum startDatum = new Datum("<s>", "O");
+    Datum endDatum = new Datum("</s>", "O");
+    data.add(startDatum);
 		for (String line = in.readLine(); line != null; line = in.readLine()) {
 			if (line.trim().length() == 0) {
+        data.add(endDatum);
+        data.add(startDatum);
 				continue;
 			}
 			String[] bits = line.split("\\s+");
@@ -45,7 +49,7 @@ public class FeatureFactory {
 			Datum datum = new Datum(word, label);
 			data.add(datum);
 		}
-
+    data.add(endDatum);
 		return data;
 	}
  
@@ -53,27 +57,37 @@ public class FeatureFactory {
 	// Look up table matrix with all word vectors as defined in lecture with dimensionality n x |V|
 	static SimpleMatrix allVecs; //access it directly in WindowModel
 	public static SimpleMatrix readWordVectors(String vecFilename) throws IOException {
-		if (allVecs!=null) return allVecs;
-		return null;
-		//TODO implement this
-		//set allVecs from filename		
-
+    BufferedReader in = new BufferedReader(new FileReader(vecFilename));   
+    int m = 0;
+    for (String line = in.readLine(); line != null; line = in.readLine()) {
+			if (line.trim().length() == 0) {
+				continue;
+			}	    	
+      String[] strArr = line.split(" ");   
+      for (int n = 0; n < strArr.length; n++) {
+        String strVal = strArr[n];
+        double val = Double.parseDouble(strVal); 
+        allVecs.set(m, n, val);
+      }
+      m++;
+    } 
+    return allVecs;
 	}
 	// might be useful for word to number lookups, just access them directly in WindowModel
 	public static HashMap<String, Integer> wordToNum = new HashMap<String, Integer>(); 
 	public static HashMap<Integer, String> numToWord = new HashMap<Integer, String>();
 
 	public static HashMap<String, Integer> initializeVocab(String vocabFilename) throws IOException {
-		//TODO: create this
+    BufferedReader in = new BufferedReader(new FileReader(vocabFilename));
+    int index = 0;
+    for (String line = in.readLine(); line != null; line = in.readLine()) {
+			if (line.trim().length() == 0) {
+				continue;
+			}	    	
+      wordToNum.put(line, index); 
+      numToWord.put(index, line);
+      index++;
+    }
 		return wordToNum;
 	}
- 
-
-
-
-
-
-
-
-
 }
