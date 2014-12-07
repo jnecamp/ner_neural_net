ab8d52c0a6f6b12f62b96bb1a88aef5bb646fd07
jnecamp <jnecamp5@gmail.com>
2014-12-06 22:54:51 -0800

    Finish changes needed for t-sne

--- tSNE_matlab/visualize_word_vecs.m
+++ tSNE_matlab/visualize_word_vecs.m
@@ -1,17 +1,21 @@
 
 % L is a  |V| x n matrix
-L = dlmread('../data/wordMatrices/origWordVectors.txt');
+%L = dlmread('../data/wordMatrices/origWordVectors.txt');
+learnedL = dlmread('../data/wordMatrices/learnedWordVectors.txt');
 
 
-relevantIndices = dlmread('relevant_indices.txt');
-relevantIndices = round(relevantIndices + 1);
+indecesInLandLabels = dlmread('trainData_indeces_and_labels.txt');
+indeces = indecesInLandLabels(:,1)+1;
+labels = indecesInLandLabels(:,2);
 
-newL = L(relevantIndices, :);
+%relevantL = L(indeces, :);
+relevantLearnedL = learnedL(indeces, :);
 
 no_dims = 2;
 init_dims = 30;
-ratio_landmarks = .1;
 perplexity = 30;
 
 % function ydata = tsne(X, labels, no_dims, initial_dims, perplexity)
-mappedX = tsne(newL, [], no_dims, init_dims, perplexity);
+%mappedX = tsne(relevantL, [], no_dims, init_dims, perplexity);
+mappedX = tsne(relevantLearnedL, [], no_dims, init_dims, perplexity);
+dlmwrite('./tsne_vecs_learned_L.txt', mappedX);
